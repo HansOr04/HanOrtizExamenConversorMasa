690dc453b5acd75a74b3c4a9272b3c7e13b775cd
HansOr04 <hansalazar04@gmail.com>
2024-12-02 10:47:04 -0500

    App y main

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,26 +1,4 @@
-<!-- MainPage.xaml -->
-<?xml version="1.0" encoding="utf-8" ?>
-<ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
-             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             xmlns:views="clr-namespace:HanOrtizExamenConversorMasa.Views"
-             x:Class="HanOrtizExamenConversorMasa.MainPage">
-    <views:HOConversionPage />
-</ContentPage>
 
-<!-- App.xaml.cs -->
-namespace HanOrtizExamenConversorMasa
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-            MainPage = new MainPage();
-        }
-    }
-}
-
-<!-- MainPage.xaml.cs -->
 namespace HanOrtizExamenConversorMasa
 {
     public partial class MainPage : ContentPage
